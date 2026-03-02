d003a6316f37c0d4f1010efcbd6a67db3b10653c
khalafkristoffer <khalafkristoffer@gmail.com>
2026-03-02 19:00:32 +0100

    initial layout

---
+++ src/lib/components/Card.svelte
@@ -0,0 +1,13 @@
+<div class="card">
+    <slot />
+</div>
+
+<style>
+    .card {
+        background: var(--bg-card);
+        border: 1px solid var(--border);
+        padding: 16px;
+        border-radius: 8px;
+        margin-bottom: 16px;
+    }
+</style>
